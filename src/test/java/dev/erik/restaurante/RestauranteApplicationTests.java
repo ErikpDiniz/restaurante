55df86bca000e2c37529046b9ad7ef8f2845e903
Erik <erikdiniz2020@icloud.com>
2026-05-18 13:52:38 -0300

    Commit de integração

---
+++ src/test/java/dev/erik/restaurante/RestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.erik.restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
